b5d1c8f26bc35d1ac60bbb1c73196b61b47b8e8b
cigon <yoosung@ultracaption.net>
2016-12-22 15:47:09 +0900

    42company-page

---
+++ Gemfile
@@ -0,0 +1,20 @@
+# If you do not have OpenSSL installed, update
+# the following line to use 'http://' instead
+source 'https://rubygems.org'
+
+gem 'slim', '>= 3.0'
+gem 'bourbon'
+
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+# windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw]
+
+# Middleman Gems
+gem 'middleman-sprockets', '>= 4.0.0.rc.1'
+gem 'middleman-compass', '>= 4.0.0.rc.1'
+gem 'middleman', '>= 4.0.0.rc.1'
+gem 'middleman-livereload'
+gem 'middleman-s3_sync'
+gem 'bh', '~> 1.3'
---
+++ config.rb
@@ -0,0 +1,47 @@
+require 'slim'
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
+
+###
+# Helpers
+###
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+# Build-specific configuration
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = ENV['AWS_BUCKET'] # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = ENV['AWS_REGION']     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key      = ENV['AWS_ACCESS_KEY_KEY']
+end
+activate :bh
+configure :build do
+
+end
